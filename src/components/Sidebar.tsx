8dccd4ea0f244679e3055213ddfa59c5983ba76c
[dyad] <git@dyad.sh>
2025-08-09 15:18:57 +0530

    [dyad] Simplified sidebar navigation - wrote 1 file(s)

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -67,7 +67,6 @@ const Sidebar = () => {
         
         <div className="space-y-1 pt-2">
           <p className="px-3 text-xs font-semibold text-gray-500 uppercase tracking-wider">Income</p>
-          <NavLink to="/tax-dashboard" className={incomeNavLinkClass} end><LayoutDashboard className="mr-3 h-4 w-4" />Summary</NavLink>
           <a href="/tax-dashboard#salary-income" className={hashLinkClass}><Wallet className="mr-3 h-4 w-4" />Salary</a>
           <NavLink to="/rental-income" className={incomeNavLinkClass}><Home className="mr-3 h-4 w-4" />Rental</NavLink>
           <NavLink to="/fd-income" className={incomeNavLinkClass}><Landmark className="mr-3 h-4 w-4" />FD</NavLink>
