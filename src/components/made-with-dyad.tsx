4fca1ab47372a6235e33b9422e3172564fb50712
[dyad] <jarvis@bravetux.com>
2025-08-10 12:56:25 +0530

    [dyad] Added GitHub repository link - wrote 1 file(s)

--- src/components/made-with-dyad.tsx
+++ src/components/made-with-dyad.tsx
@@ -1,6 +1,6 @@
 export const MadeWithDyad = () => {
   return (
-    <div className="p-4 text-center">
+    <div className="p-4 text-center flex flex-col items-center space-y-1">
       <a
         href="https://github.com/bravetux"
         target="_blank"
@@ -9,6 +9,17 @@ export const MadeWithDyad = () => {
       >
         Designed by Bravetux
       </a>
+      <p className="text-sm text-gray-500 dark:text-gray-400">
+        This website is published in{" "}
+        <a
+          href="https://github.com/bravetux/NewTax_Computation"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline hover:text-gray-700 dark:hover:text-gray-200"
+        >
+          GitHub
+        </a>
+      </p>
     </div>
   );
 };
